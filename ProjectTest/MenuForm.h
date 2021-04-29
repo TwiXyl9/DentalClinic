c31bbe064a9790ff9ffd5fef179932ace88571f4
TwiXyl9 <kolkin.2002@mail.ru>
2021-04-30 00:18:17 +0300

    переделать стартовую форму под таблицу для дальнейшего вывода данных

--- ProjectTest/MenuForm.h
+++ ProjectTest/MenuForm.h
@@ -36,11 +36,12 @@ namespace ProjectTest {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
+
 	private: System::Windows::Forms::Button^ PatientBase_Button;
 
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::ListBox^ listBox1;
 	protected:
 
 	protected:
@@ -59,28 +60,18 @@ namespace ProjectTest {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MenuForm::typeid));
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->PatientBase_Button = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
-			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(200, 80);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(100, 50);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"MENU";
-			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// PatientBase_Button
 			// 
+			this->PatientBase_Button->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->PatientBase_Button->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->PatientBase_Button->Location = System::Drawing::Point(175, 152);
+			this->PatientBase_Button->Location = System::Drawing::Point(12, 70);
 			this->PatientBase_Button->Name = L"PatientBase_Button";
 			this->PatientBase_Button->Size = System::Drawing::Size(150, 35);
 			this->PatientBase_Button->TabIndex = 1;
@@ -90,7 +81,7 @@ namespace ProjectTest {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(175, 221);
+			this->button2->Location = System::Drawing::Point(168, 71);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(150, 35);
 			this->button2->TabIndex = 2;
@@ -99,27 +90,34 @@ namespace ProjectTest {
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(175, 284);
+			this->button3->Location = System::Drawing::Point(324, 71);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(150, 35);
 			this->button3->TabIndex = 3;
 			this->button3->Text = L"button3";
 			this->button3->UseVisualStyleBackColor = true;
 			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 16;
+			this->listBox1->Location = System::Drawing::Point(12, 122);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(834, 404);
+			this->listBox1->TabIndex = 4;
+			// 
 			// MenuForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(482, 553);
+			this->ClientSize = System::Drawing::Size(858, 553);
+			this->Controls->Add(this->listBox1);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->PatientBase_Button);
-			this->Controls->Add(this->label1);
 			this->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
-			this->MaximumSize = System::Drawing::Size(500, 600);
-			this->MinimumSize = System::Drawing::Size(500, 600);
 			this->Name = L"MenuForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Dental Clinic";
@@ -129,7 +127,9 @@ namespace ProjectTest {
 #pragma endregion
 	private: System::Void PatientBase_Button_Click(System::Object^ sender, System::EventArgs^ e) {
 		PatientBaseForm patientBaseForm;
-		patientBaseForm.Show();
+		this->Hide();
+		patientBaseForm.ShowDialog();
+		this->Show();
 	}
 };
 }
--- ProjectTest/PatientBaseForm.cpp
+++ ProjectTest/PatientBaseForm.cpp
@@ -1,9 +1,9 @@
 #include "PatientBaseForm.h"
 #include <Windows.h>
 using namespace ProjectTest; // Название проекта
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew PatientBaseForm);
-	return 0;
-}
+//int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+//	Application::EnableVisualStyles();
+//	Application::SetCompatibleTextRenderingDefault(false);
+//	Application::Run(gcnew PatientBaseForm);
+//	return 0;
+//}
